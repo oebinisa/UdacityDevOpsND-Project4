b8b84ea8d8ade258f11b84fe65e75ffa3ea652a1
OEbinisa <o.ebinisa@gmail.com>
2023-03-12 14:59:13 +0100

    Final 01

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=oebinisa/devops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run devops\
+    --image=$dockerpath\
+    --port=80 --labels app=devops
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward devops 8000:80
